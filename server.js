97cb8ced9af5ddc1d1ef5c50a47966b3074776f6
Gilles Vivies <gilles.vivies@orange.com>
2014-06-06 23:44:18 +0200

    Jenkins dashboard on main page

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ var db = mongoose.connect(config.db);
 var app = require('./server/config/system/bootstrap')(passport, db);
 
 // Start the app by listening on <port>, optional hostname
-app.listen(config.port, config.hostname);
+// [var server =] added for cron start below
+var server = app.listen(config.port, config.hostname);
+
 console.log('Mean app started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
 
 // Initializing logger
@@ -28,3 +30,18 @@ logger.init(app, passport, mongoose);
 
 // Expose app
 exports = module.exports = app;
+
+/* =================================================================== *
+ * Initializing cron jobs for JENKINS plugin                           *
+ * =================================================================== */
+var io = require('socket.io');
+io = io.listen(server);
+console.log('Socket.io initialized');
+var cron = require('cron');
+var monitoring = require('./packages/jenkins/server/controllers/monitoring');
+var cronJob = cron.job('*/5 * * * * *', function(){
+		monitoring.checkJobs(io);
+}); 
+cronJob.start();
+console.log('Cron job started');
+/* =================================================================== */
